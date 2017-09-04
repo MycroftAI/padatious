b0916108ac149bebc6e2ec595767eac04f9d44e1
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-09-03 20:39:50 -0500

    Fix formatting

--- padatious/match_data.py
+++ padatious/match_data.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class MatchData(object):
     """
     A set of data describing how a query fits into an intent
