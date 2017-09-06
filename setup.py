46716835573a8aa5edc376c5cad338f9d4d974c8
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-09-06 14:35:07 -0500

    Incremement version to 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='padatious',
-    version='0.2.1',  # Also change in padatious/__init__.py
+    version='0.2.2',  # Also change in padatious/__init__.py
     description='A neural network intent parser',
     url='http://github.com/MycroftAI/padatious',
     author='Matthew Scholefield',
